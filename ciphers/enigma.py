0b517d4ad6336d986f6989a0072a9f402044ae16
Scott F <98131933+SF-11@users.noreply.github.com>
2022-03-03 21:24:54 -0500

    enigma stepping fixed, individual chars still not encoding correctly

--- ciphers/enigma.py
+++ ciphers/enigma.py
@@ -32,17 +32,16 @@ enigma_I_rotor_alphabets = {
 reflector_b = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 
 
+s1 = 15
+s2 = 4
 s3 = 1
-s2 = 1
-s1 = 1
 
 
 
-rotors = [enimga_1_rotor_III, enigma_1_rotor_II, enigma_1_rotor_I]
-rotor_states = [s3, s2, s1]
+rotors = [enigma_1_rotor_I, enigma_1_rotor_II, enimga_1_rotor_III]
+rotor_states = [s1, s2, s3]
 
 def enc_letter(letter):
-    print(letter)
     """
     x = enimga_1_rotor_III[alpha.index(letter) + s3-1]
     print(x)
@@ -62,58 +61,90 @@ def enc_letter(letter):
 
     x = letter
     for i in range(len(rotors)):
-        x = rotors[i][alpha.index(x) + rotor_states[i]-1]
-        print(x)
+        x = rotors[i][(alpha.index(x) + rotor_states[i]-1)%26]
 
     x = reflector_b[alpha.index(x)]
-    print(x)
 
     for i in range(len(rotors)):
-        x = alpha[rotors[2-i].index(x) + rotor_states[2-i]-1]
-        print(x)
+        x = alpha[(rotors[2-i].index(x) + rotor_states[2-i]-1)%26]
+
+    return x
 
 
 
 class Rotor():
 
-    def __init__(self, state: int=1, notch: int=26):
+    def __init__(self, alphabet, notch: int=26, state: int=1):
+        self.alphabet = alphabet
         self.state = state
         self.notch = notch
         self.next = None
         
 
-    def step(self):
-        self.state += 1
-        if self.state == 27: self.state = 1
+    def step(self, notched=True):
+        
+        if self.next:
+            self.next.step(notched=self.state == self.notch)
 
-        if self.state == self.notch and self.next != None:
-            self.next.step()
-
+        if notched or self.state == self.notch:
+            self.state = ((self.state) % 26) + 1 # increment and 1-index modulo 
+        
         return self.state
 
 
 
-class Engima():
-    pass
+class Enigma():
+    
+    rotor_alphabets = {
+        "I":   ("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"),
+        "II":  ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"),
+        "III": ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
+    }
 
-    def __init__(self, rotor_states):
-        self.rotors = [Rotor(rotor_states[i]) for i in rotor_states]
+
+    def __init__(self, rotors, rotor_states):
+        self.rotors = []
+        for i in range(len(rotors)):
+            r = self.rotor_alphabets[rotors[i]]
+            self.rotors.append(Rotor(r[0], alpha.index(r[1])+1, rotor_states[i]))
+
+        for i in range(len(rotors)-1):
+            self.rotors[i].next = self.rotors[i+1]
+
+
+    def encrypt(self, text: str):
+        ret_text = ""
+        print(alpha[self.rotors[2].state-1], alpha[self.rotors[1].state-1], alpha[self.rotors[0].state-1])
+        for char in text:
+            ret_text += self.enc_letter(char)
+            self.rotors[0].step()
+            print(alpha[self.rotors[2].state-1], alpha[self.rotors[1].state-1], alpha[self.rotors[0].state-1])
+
+        return ret_text
+
+
+
+    def enc_letter(self, letter):
+        x = letter
+        for i in range(len(self.rotors)):
+            r = self.rotors[i]
+            x = r.alphabet[(alpha.index(x) + r.state-1)%26]
+ 
+        x = reflector_b[alpha.index(x)]
+
+        for i in range(0, len(rotors), -1):
+            r = self.rotors[i]
+            x = alpha[(r.alphabet.index(x) + r.state-1)%26]
+
+        return x
+
+
 
 
 if __name__ == "__main__":
     
-    r1 = Rotor(state=15, notch=17)
-    r2 = Rotor(state=4, notch=5)
-    r3 = Rotor(state=1, notch=22)
-    
 
-    r1.next = r2
-    r2.next = r3
+    enigma = Enigma(["I", "II", "III"], (15,4,1))
+    print(enigma.encrypt("TEST"))
 
-    print(alpha[r3.state-1], alpha[r2.state-1], alpha[r1.state-1])
-    for i in range(10):
-        r1.step()
-        print(alpha[r3.state-1], alpha[r2.state-1], alpha[r1.state-1])
-
-    print()    
-    enc_letter("A")
+    print(enc_letter("T"))
